e174c97b2fa176ffacca0aa8904c918f32e734db
Mark Bain <markbaindesign@gmail.com>
2025-03-13 12:12:36 +0100

    refactor admin

--- public_html/wp-content/plugins/bd-search/inc/inc.php
+++ public_html/wp-content/plugins/bd-search/inc/inc.php
@@ -4,7 +4,9 @@ if (!defined('ABSPATH')) {
    die('Invalid request, dude.');
 }
 
-require_once BD616__PLUGIN_DIR . '/inc/admin.php';
+require_once BD616__PLUGIN_DIR . '/vendor/autoload.php';
+
+require_once '/admin/admin.php';
 require_once BD616__PLUGIN_DIR . '/inc/algolia/algolia.php';
 require_once BD616__PLUGIN_DIR . '/inc/form.php';
 require_once BD616__PLUGIN_DIR . '/inc/modal.php';
@@ -13,4 +15,3 @@ require_once BD616__PLUGIN_DIR . '/inc/shortcodes.php';
 require_once BD616__PLUGIN_DIR . '/inc/styles.php';
 require_once BD616__PLUGIN_DIR . '/inc/wpml.php';
 require_once BD616__PLUGIN_DIR . '/inc/templates/templates.php';
-require_once BD616__PLUGIN_DIR . '/vendor/autoload.php';
