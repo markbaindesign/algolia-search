54edc624b12ca7c13ca2ce322ddf22f4e46946a5
Mark Bain <markbaindesign@gmail.com>
2025-03-13 18:01:16 +0100

    refactor global script loader

--- public_html/wp-content/plugins/bd-search/inc/scripts/scripts.php
+++ public_html/wp-content/plugins/bd-search/inc/scripts/scripts.php
@@ -8,6 +8,4 @@ if (!defined('ABSPATH')) {
 require_once 'functions/get_script_handles.php';
 require_once 'functions/load_scripts.php';
 require_once 'functions/load_default_scripts.php';
-
-/* Load custom scripts */
-require_once BD616__PLUGIN_DIR . '/inc/scripts/algolia-scripts-custom.php';
+require_once 'functions/register_algolia_script_global.php';
