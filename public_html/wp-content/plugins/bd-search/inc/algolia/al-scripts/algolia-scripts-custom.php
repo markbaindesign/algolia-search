1c0f3e78fc705e92c1f419a0931f153bca701b15
Mark Bain <markbaindesign@gmail.com>
2025-03-13 17:58:48 +0100

    remove duplicate global script loader

--- public_html/wp-content/plugins/bd-search/inc/algolia/al-scripts/algolia-scripts-custom.php
+++ public_html/wp-content/plugins/bd-search/inc/algolia/al-scripts/algolia-scripts-custom.php
@@ -6,11 +6,5 @@ if (!defined('ABSPATH')) {
    die('Invalid request, dude.');
 }
 
-/* Load script - Global */
-require_once BD616__PLUGIN_DIR . '/inc/algolia/scripts/scripts-custom/register-global-script.php';
-
-/* Load script - Map */
-require_once BD616__PLUGIN_DIR . '/inc/algolia/scripts/scripts-custom/register-global-script.php';
-
 /* Load script - Modal */
 require_once BD616__PLUGIN_DIR . '/inc/algolia/scripts/scripts-custom/register-modal-script.php';
