ca5135a1196549bbf463c1e2988a699d8b5e0caa
Mark Bain Design <markbaindesign@gmail.com>
2025-05-02 19:03:59 +0200

    Refactor search template function

--- public_html/wp-content/plugins/bd-search/inc/templates/functions/show-search-template.php
+++ public_html/wp-content/plugins/bd-search/inc/templates/functions/show-search-template.php
@@ -10,11 +10,11 @@ if (!defined('ABSPATH')) {
  * A template part to display a search box and a list of results,
  * with filters.
  * 
- * @param $search_index       The index to search
+ * @param $index_name       The index to search
  * @param $template_name       The template name
  */
 if (!function_exists('bd324_show_search_template')):
-   function bd324_show_search_template($search_index, $template_name = '')
+   function bd324_show_search_template($index_name, $template_name = '')
    {
       $output = '';
 
@@ -24,100 +24,106 @@ if (!function_exists('bd324_show_search_template')):
       }
 
       // Enqueue Index Scripts
-      $handle_script = bd324_get_script_handles($search_index, $template_name);
+      $handle_script = bd324_get_script_handles($index_name, $template_name);
       bd324_enqueue_script_if_registered($handle_script);
 
-      $handle_script_config = bd324_get_script_handles($search_index, $template_name, true);
+      $handle_script_config = bd324_get_script_handles($index_name, $template_name, true);
       bd324_enqueue_script_if_registered($handle_script_config);
 
       // Open Wrapper
       $wrapper_open = apply_filters(
-         'BD616__filter_algolia_template_wrapper' . $search_index . '_' . $template_name,
-         '<div id="ais-wrapper" class="bd-search search__wrapper search-wrapper search-fullpage search-wrapper--listings search-wrapper--' . $search_index . ' search-wrapper--' . $template_name . '">',
-         $search_index
+         'BD616__filter_algolia_template_wrapper_' . $index_name . '_' . $template_name,
+         '<div id="ais-wrapper" class="bd-search search__wrapper search-wrapper search-fullpage search-wrapper--listings search-wrapper--' . $index_name . ' search-wrapper--' . $template_name . '">',
+         $index_name
       );
       $output .= $wrapper_open;
 
       // Start Template Part - Header
       $output .= apply_filters(
-         'BD616__filter_algolia_template_header' . $search_index . '_' . $template_name,
+         'BD616__filter_algolia_template_header_' . $index_name . '_' . $template_name,
          '<header id="ais-header" class="search__header">',
-         $search_index
+         $index_name
       );
 
       // Search
-      $search = bd324_get_algolia_template_part('searchbox', $search_index, $template_name);
+      $search = bd324_get_algolia_template_part('searchbox', $index_name, $template_name);
       $output .= $search;
 
       $output .= apply_filters(
-         'BD616__filter_algolia_template_header_close' . $search_index . '_' . $template_name,
+         'BD616__filter_algolia_template_header_close_' . $index_name . '_' . $template_name,
          '</header>',
-         $search_index
+         $index_name
       );
       // End Template Part - Aside
 
       // Start Template Part - Aside
-      $output .= apply_filters(
-         'BD616__filter_algolia_template_aside' . $search_index . '_' . $template_name,
+      $aside = '';
+      $aside .= apply_filters(
+         'BD616__filter_algolia_template_aside_open_' . $index_name . '_' . $template_name,
          '<nav id="ais-aside" class="search__aside">',
-         $search_index
+         $index_name
       );
 
       // Stats
-      $stats = bd324_get_algolia_template_part('stats', $search_index, $template_name);
-      $output .= $stats;
+      $stats = bd324_get_algolia_template_part('stats', $index_name, $template_name);
+      $aside .= $stats;
 
       // Clear Filters
-      $filters_clear = bd324_get_algolia_template_part('filters_clear', $search_index, $template_name);
-      $output .= $filters_clear;
+      $filters_clear = bd324_get_algolia_template_part('filters_clear', $index_name, $template_name);
+      $aside .= $filters_clear;
 
       // Filters
-      $filters = bd324_get_algolia_template_part('filters', $search_index, $template_name);
-      $output .= $filters;
+      $filters = bd324_get_algolia_template_part('filters', $index_name, $template_name);
+      $aside .= $filters;
 
-      $output .= apply_filters(
-         'BD616__filter_algolia_template_aside_close' . $search_index . '_' . $template_name,
+      $aside .= apply_filters(
+         'BD616__filter_algolia_template_aside_close_' . $index_name . '_' . $template_name,
          '</nav>',
-         $search_index
+         $index_name
+      );
+      $output .= apply_filters(
+         'BD616__filter_algolia_template_aside_' . $index_name . '_' . $template_name,
+         $aside,
+         $index_name
       );
       // End Template Part - Aside
 
       // Start Template Part - Main
       $output .= apply_filters(
-         'BD616__filter_algolia_template_main' . $search_index . '_' . $template_name,
+         'BD616__filter_algolia_template_main_' . $index_name . '_' . $template_name,
          '<main id="ais-main" class="search__main">',
-         $search_index
+         $index_name
       );
 
       // Current Filters
-      $filters_current = bd324_get_algolia_template_part('filters_current', $search_index, $template_name);
+      $filters_current = bd324_get_algolia_template_part('filters_current', $index_name, $template_name);
       $output .= $filters_current;
 
       // Hits
-      $hits = bd324_get_algolia_template_part('hits', $search_index, $template_name);
+      $hits = bd324_get_algolia_template_part('hits', $index_name, $template_name);
       $output .= $hits;
 
       $output .= apply_filters(
-         'BD616__filter_algolia_template_main_close' . $search_index . '_' . $template_name,
+         'BD616__filter_algolia_template_main_close_' . $index_name . '_' . $template_name,
          '</main>',
-         $search_index
+         $index_name
       );
       // End Template Part - Main
 
       // Close Wrapper
       $wrapper_close = apply_filters(
-         'BD616__filter_algolia_template_wrapper_close' . $search_index . '_' . $template_name,
+         'BD616__filter_algolia_template_wrapper_close_' . $index_name . '_' . $template_name,
          '</div>',
-         $search_index
+         $index_name
       );
       $output .= $wrapper_close;
 
       // Output
       // Filter allows reordering/removal of elements in template
       echo apply_filters(
-         'BD616__filter_algolia_template_output_' . $search_index . '_' . $template_name,
+         'BD616__filter_algolia_template_output_' . $index_name . '_' . $template_name,
          $output,
-         $search_index,
+         $index_name,
          $wrapper_open,
          $search,
          $stats,
